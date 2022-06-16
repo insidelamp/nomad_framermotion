2148ba5ade4b124e870b5342b4e9fa311b47f2d1
insidelamp <rkdhfh@naver.com>
2022-06-16 17:16:03 +0900

    기본 animation작업

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,4 +1,5 @@
 import styled from "styled-components";
+import { motion } from "framer-motion";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -8,18 +9,26 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled.div`
+const Box = styled(motion.div)`
   width: 200px;
   height: 200px;
   background-color: white;
-  border-radius: 10px;
+  border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
+
+//framer 에서 styled-components 사용법
+// const Box = styled.div`` <- 예전 사용법
+// const Box = styled(motion.div)``  <- framer 에서 styled-components 사용법
 
 function App() {
   return (
     <Wrapper>
-      <Box />
+      <Box
+        transition={{ type: "spring", delay: 0.5 }}
+        initial={{ scale: 0 }}
+        animate={{ scale: 1, rotate: 360 }}
+      />
     </Wrapper>
   );
 }
--- animation/src/craco.config.js
+++ animation/src/craco.config.js
@@ -13,4 +13,5 @@ module.exports = {
     },
   },
 };
-// script 대체
+// crate-react-app에 craco <- script 대체
+//  crate-react-app  EcmaSript 자바스크립트 모듈이해하게하기
