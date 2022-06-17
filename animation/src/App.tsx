7a4e756610cbc7054a7b5e3382ef133c78e317e9
insidelamp <rkdhfh@naver.com>
2022-06-17 11:26:57 +0900

    useMotionValue 렌더링x 값확인

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,6 +1,6 @@
 import styled from "styled-components";
-import { motion } from "framer-motion";
-import { useRef } from "react";
+import { motion, useMotionValue } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 const Wrapper = styled.div`
   height: 100vh;
@@ -18,42 +18,20 @@ const Box = styled(motion.div)`
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const boxVariants = {
-  hover: { scale: 1.5, rotateZ: 90 },
-  click: { scale: 1, borderRadius: "100px" },
-};
-
-const BiggerBox = styled.div`
-  width: 600px;
-  height: 600px;
-  background-color: rgba(255, 255, 255, 0.4);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-`;
-
 function App() {
-  const biggerBoxRef = useRef<HTMLDivElement>(null);
+  const x = useMotionValue(0);
+  useEffect(() => {
+    x.onChange(() => console.log(x.get()));
+  }, [x]);
+  console.log(x);
   return (
     <Wrapper>
-      <BiggerBox ref={biggerBoxRef}>
-        <Box
-          drag
-          dragSnapToOrigin
-          dragElastic={0.5}
-          dragConstraints={biggerBoxRef}
-          variants={boxVariants}
-          whileHover="hover"
-          whileTap="click"
-        />
-      </BiggerBox>
+      <button onClick={() => x.set(200)}>click me</button>
+      <Box style={{ x }} drag="x" dragSnapToOrigin />
     </Wrapper>
   );
 }
 
 export default App;
 
-//dragConstraints = 제약이있는 box를 만들어줌 드래깅이 허용될수있는 영역 위,아래,왼쪽,오른쪽 설정할시 중간으로 돌아오게 만들수도있음
-// dragSnapToOrigin = 중앙 원래위치로  돌아가게해줌
-// dragElastic (드래그탄성 ) =
+// motionValue 는 에니메이션값의 상태와 속도를 추적해줌 ( div를 추적해줌 )
