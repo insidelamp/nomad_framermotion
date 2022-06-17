97b1dae5484c89c2d0ef74374be80349ddbaf636
insidelamp <rkdhfh@naver.com>
2022-06-18 08:54:36 +0900

    nomad challenge clone

--- animation/src/App.tsx
+++ animation/src/App.tsx
@@ -1,45 +1,26 @@
-import styled from "styled-components";
-import { AnimatePresence, motion } from "framer-motion";
-import { useState } from "react";
-
-const Wrapper = styled(motion.div)`
-  height: 100vh;
-  width: 100vw;
-  display: flex;
-  justify-content: space-around;
-  align-items: center;
-`;
-
-const Box = styled(motion.div)`
-  width: 400px;
-  height: 400px;
-  background-color: rgba(255, 255, 255, 1);
-  border-radius: 40px;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-  overflow: hidden;
-`;
-const Circle = styled(motion.div)`
-  background-color: #00a5ff;
-  height: 100px;
-  width: 100px;
-  border-radius: 50px;
-  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
-`;
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Home from "./Routes/Home";
+import Search from "./Routes/Search";
+import Tv from "./Routes/Tv";
+import Header from "./Components/Header";
 
 function App() {
-  const [clicked, setClicked] = useState(false);
-  const toggleClicked = () => setClicked((prev) => !prev);
   return (
-    <Wrapper onClick={toggleClicked}>
-      <Box>{!clicked ? <Circle layoutId="circle" /> : null}</Box>
-      <Box>{clicked ? <Circle layoutId="circle" /> : null}</Box>
-    </Wrapper>
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/">
+          <Home />
+        </Route>
+        <Route path="/tv">
+          <Tv />
+        </Route>
+        <Route path="search">
+          <Search />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
 export default App;
-
-//AnimatePresence 의 custo은 variants 에 데이터를 보낼수있게해주는 property 임 예로 가고자하는 방향을따라 invisible과 exit를 바꾸면됨
--- animation/src/index.tsx
+++ animation/src/index.tsx
@@ -3,8 +3,7 @@ import ReactDOM from "react-dom";
 import { RecoilRoot } from "recoil";
 import { ThemeProvider } from "styled-components";
 import App from "./App";
-import { darkTheme } from "./theme";
-
+import { theme } from "./theme";
 import { createGlobalStyle } from "styled-components";
 
 const GlobalStyle = createGlobalStyle`
@@ -74,7 +73,7 @@ a {
 ReactDOM.render(
   <React.StrictMode>
     <RecoilRoot>
-      <ThemeProvider theme={darkTheme}>
+      <ThemeProvider theme={theme}>
         <GlobalStyle />
         <App />
       </ThemeProvider>
